2f74b99a3a51c9622155b485102429f14be75fea
MadJoker33 <behlingid19@uww.edu>
2023-02-04 23:17:57 -0600

    fixed footstep audio

--- Assets/scripts/audioscript.cs
+++ Assets/scripts/audioscript.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class audioscript : MonoBehaviour
 {
-
+    [SerializeField] private LayerMask Floor;
+    [SerializeField] private Transform Feet;
     public AudioSource footstepSound;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-        {
-            footstepSound.enabled = true;
 
-        }
-        else
-        {
-            footstepSound.enabled = false;
-        }
+       
+        
+
+
+            if (Input.GetKey(KeyCode.W) && Physics.CheckSphere(Feet.position, 0.1f, Floor) || Input.GetKey(KeyCode.A) && Physics.CheckSphere(Feet.position, 0.1f, Floor) || Input.GetKey(KeyCode.S) && Physics.CheckSphere(Feet.position, 0.1f, Floor) || Input.GetKey(KeyCode.D) && Physics.CheckSphere(Feet.position, 0.1f, Floor))
+            {
+                footstepSound.enabled = true;
+
+            }
+            else
+            {
+                footstepSound.enabled = false;
+            }
+        
         
     }
 }
