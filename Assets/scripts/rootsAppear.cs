48c0f4d044e9e86c76ee7bca9238163106ba4f88
Darktemplar33 <isaacbehling2000@gmail.com>
2023-02-05 13:49:50 -0600

    done

--- Assets/scripts/rootsAppear.cs
+++ Assets/scripts/rootsAppear.cs
@@ -11,36 +11,57 @@ public class rootsAppear : MonoBehaviour
     public GameObject floorRoots2;
     public GameObject floorRoots3;
     public GameObject floorRoots4;
-    
+    public GameObject floorRoots5;
+    public GameObject floorRoots6;
+    public GameObject floorRoots7;
+
     // Update is called once per frame
     void Update()
     {
 
-        if (timerScript.currentTime <= 60 )
+        if (timerScript.currentTime <= 30 )
         {
             floorRoots.SetActive(true);
             
 
         }
-        if (timerScript.currentTime <= 120 )
+        if (timerScript.currentTime <= 70)
+        {
+            floorRoots5.SetActive(true);
+
+
+        }
+        if (timerScript.currentTime <= 110)
+        {
+            floorRoots6.SetActive(true);
+
+
+        }
+        if (timerScript.currentTime <= 140)
+        {
+            floorRoots7.SetActive(true);
+
+
+        }
+        if (timerScript.currentTime <= 170 )
         {
             floorRoots4.SetActive(true);
             
 
         }
-        if (timerScript.currentTime <= 180 )
+        if (timerScript.currentTime <= 200 )
         {
             floorRoots3.SetActive(true);
             
 
         }
-        if (timerScript.currentTime <= 240 )
+        if (timerScript.currentTime <= 230 )
         {
             floorRoots2.SetActive(true);
             
 
         }
-        if (timerScript.currentTime <= 300 )
+        if (timerScript.currentTime <= 260 )
         {
             floorRoots1.SetActive(true);
             
